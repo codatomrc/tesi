ec787144d9895802b292b976ab053095ea8b7705
codatomrc <94675753+codatomrc@users.noreply.github.com>
2022-04-13 12:46:54 +0200

    Loaded files

---
+++ fits_reader.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import astropy.coordinates as coord
+from astropy import units as u
+from astroquery.simbad import Simbad
+from scipy.signal import find_peaks, peak_widths
+
+hdul = fits.open('pippo.fits')
+hdr = hdul[0].header
+
+print(hdr)
+
+
+LAMBDA0 = hdr['CRVAL1']
+DELTA = hdr['CDELT1']
+NAXIS1 = hdr['NAXIS1']
+LAMBDA = np.arange(LAMBDA0, LAMBDA0+NAXIS1*DELTA, DELTA)
+
+data = hdul[0].data
+
+plt.imshow(data)
+plt.show()
