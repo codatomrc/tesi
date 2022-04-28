05be810757e8e0d1cb57c16cb42f3df958d1fb81
codatomrc <marcocodato03@gmail.com>
2022-04-28 15:31:15 +0200

    Daily update 28 Apr

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ I am going to update my work daily during the week.
 ## Roadmap
 
 ### Done (from the latest)
+- I improved the bkg detection and extraction fit. Now It computed the systematic bkg trend and removes it before compute position and width of the sources.
 - I sketched some code that bins each spectrum and compares the frames, chornologically ordered.
 - I am starting developing a new script that reads the bkg files generated with the first one.
 - **Bkg extraction**. I wrote a script that automatically read and process all the files in a directory.
--- bkg_extractor.py
+++ bkg_extractor.py
@@ -6,23 +6,28 @@ from datetime import datetime
 import glob
 import os
 from wotan import flatten
+from scipy.optimize import curve_fit
 
 ######################
 #OPTIONS
-savefiles = False
+save_plots = False
+save_FITS = False
 plot_profile = True
-plot_spec = True
-show_ima = True
+plot_spec = False
+show_ima = False
 
 #PARAMS
+peak_height = 0.05 #height abouve the bkg level
 data_col_frac = .75 #minimum fraction of valid pixels in a column
-width_mult = 3.5 # interval to exclude around a source, wrt center in FWHM units
+width_mult = 2 # interval to exclude around a source, wrt center in FWHM units
+cr_width = 2.5 # cr trace spatial width
+cr_prominence = 5 #treshold height wrt average column level
 cr_pad = 1 # number of px to exclude around cr, in a fixed column
 LAMBDA_lim = 3500 #A, limit blue wavelength
 ######################
 
 #browse all the *.fc.fits files in a directory and its subdirectories
-main_path = './Asiago_nightsky/2007/'
+main_path = './Asiago_nightsky/2020/'
 #main_path = './'
 file_ls = glob.glob(main_path+'/**/*.fc.fits', recursive= True)
 names = [os.path.basename(x) for x in file_ls]
@@ -32,7 +37,13 @@ f = open("bkg_extr.log", "w")
 f.write('Running bkg_extr.py at '+
         datetime.now().strftime("%H:%M:%S, %Y-%m-%d")+'\n')
 f.close()
+
+#append to the log
 warnings_count = 0
+def log_append(message):
+    f = open("bkg_extr.log", "a")
+    f.write(message+'\n')
+    f.close()
 
 ######################
 ######################
@@ -49,16 +60,17 @@ for name,file in zip(names,file_ls):
     #extract wavelenght information from the header
     NAXIS1, NAXIS2 = hdr['NAXIS1'], hdr['NAXIS2']
     LAMBDA0, DELTA = hdr['CRVAL1'], hdr['CDELT1']
-    
+
+    #generate the lambdas array
+    if hdr['CTYPE1'] != 'LINEAR':
+        log_append('WARNING: no linear wavelength calibration')    
     LAMBDA = np.arange(LAMBDA0, LAMBDA0+NAXIS1*DELTA, DELTA)
     if len(LAMBDA) == NAXIS1+1:
         LAMBDA = LAMBDA[:-1]
 
     #remove extreme blue wavelengths
-    LAMBDA_start_id = 0
-    if LAMBDA0 <= LAMBDA_lim:
-        LAMBDA_start_id = len(LAMBDA)-len(LAMBDA[ LAMBDA>LAMBDA_lim])
-        LAMBDA = LAMBDA[LAMBDA_start_id:]
+    LAMBDA_start_id = len(LAMBDA)-len(LAMBDA[ LAMBDA>LAMBDA_lim])
+    LAMBDA = LAMBDA[LAMBDA_start_id:]
         
     year = hdr['DATE-OBS'][:4]
 
@@ -70,12 +82,8 @@ for name,file in zip(names,file_ls):
         CCDSCALE = hdr['CCDSCALE'] #arcsec/px
     except KeyError:
         BINX, BINY = hdr['HBIN'], hdr['VBIN']
-    
-        warnings_count += 1
-        no_scale = ' WARNING: no scale info in the hdr (using defauls)'
-        f = open("bkg_extr.log", "a")
-        f.write(file+no_scale+'\n')
-        f.close()
+
+        log_append(' WARNING: no scale info in the hdr (using defauls)')
         
         TELSCALE = 10.70 #arcsec/mm #TO BE CHECKED!!!
         CCDSCALE = 0.60 #arcsec/px #TO BE CHECKED!!!
@@ -87,20 +95,9 @@ for name,file in zip(names,file_ls):
     #bkg level estiamtion
     raw_data = hdul[0].data[:,LAMBDA_start_id:]
     raw_integr = np.sum(raw_data, axis = 1)
-    
     x = np.arange(len(raw_integr))
-    
-    #signal detrend
-    flat,trend = flatten(
-        x,
-        raw_integr,
-        method='biweight',
-        window_length=10*SLIT_px,
-        cval = 1, return_trend = True)
-
-    #esimate the bkg level and noise amplitude
-    bkg_est = np.nanmean(trend)
-    noise = np.nanstd(raw_integr-trend)
+        
+    bkg_est = np.nanmedian(raw_integr)
 
 ######################
     #remove cosmic rays and UV noise
@@ -108,9 +105,10 @@ for name,file in zip(names,file_ls):
 
     cr_col_frac = np.zeros(len(LAMBDA)) #fraction of remaining px
     for cr_col,col in enumerate(data.T):
+        col_avg = np.nanmean(data[:,cr_col])
         cr_line,_ = find_peaks(col,
-                               prominence = bkg_est/len(raw_data[1])*5,
-                               width = (0,3))
+                               prominence = cr_prominence*col_avg,
+                               width = (0,cr_width))
 
         cr_widths = peak_widths(col, cr_line, rel_height=0.5)[0]
 
@@ -136,54 +134,62 @@ for name,file in zip(names,file_ls):
 ######################
     #use noise/bkg info to find peaks
     integr = np.nansum(data, axis = 1)
+    bkg_est = np.median(integr)
     
-    peaks,properties = find_peaks(integr, prominence=noise, width = 3)
-    peak_FWHM = peak_widths(integr, peaks, rel_height=0.5)[0]
+    #detrend: global trend (including peaks)
+    _,trend_raw = flatten (x,
+                            integr ,
+                            method ='biweight',
+                            window_length =200 ,
+                            cval = 10, return_trend = True )
 
-    #set left and right boundaries of the source region along the slit
-    left_width = peaks-peak_FWHM*width_mult
-    right_width = peaks+peak_FWHM*width_mult
+    #trim removing peaks, i.e. data fare above the global trend
+    integr_trim = np.where(integr <= trend_raw+0.05*bkg_est,
+                           integr, trend_raw)
 
+    #detrend the trimmed data, much less sensitive to the peaks
+    _,trend = flatten (x,
+                       integr_trim ,
+                       method ='biweight',
+                       window_length =50 ,
+                       cval = 10, return_trend = True )
+    '''
+    more plot about detrending
+    plt.plot(x,integr, label='original profile')
+    plt.plot(x,trend_raw, label='raw de-trend')
+    plt.plot(x,integr_trim, label='trimmed profile')
+    plt.plot(x,trend, label='final bkg trend estimation')
+    plt.legend()
+    plt.show()
+    '''
 
-######################
-    #remove overlapping ranges
+    #detrend residuals: original peaks are highlighted wrt the bkg profile
+    diff = integr-trend
+    
+    #find peaks
+    peaks,properties = find_peaks(diff, height=0.05*bkg_est, width = cr_width)
+    peak_FWHM = peak_widths(integr, peaks, rel_height=.5)[0]/2.
 
-    #compress left and right boundaries into a single array
-    widths = np.array([left_width.T, right_width.T]).T
-    widths = np.reshape(widths, 2*len(left_width))
-
-    #find and mark overlaps
-    for i in range(len(widths)-1):
-        if widths[i] > widths[i+1]:
-            widths[i] = np.nan
-            widths[i+1] = np.nan
-    #shrink the array to unmasked values
-    widths=widths[~np.isnan(widths)]
-
-    #reshape the array into the original two
-    left_width, right_width = np.reshape(widths, (int(len(widths)/2),2)).T
-
-    #remove values beyond the CCD size limits
-    try:
-        if left_width[0] < 0:
-            left_width[0] = 0
-        if right_width[-1] > NAXIS2:
-            right_width[-1] = NAXIS2
-    except IndexError:
+    if len(peak_FWHM)== 0:
         no_source = " WARNING: no sources were detected"
-        warnings_count += 1
-        f = open("bkg_extr.log", "a")
-        f.write(file+no_source)
-        f.close()
+        log_append(no_source)
 
+    #generate a boolena mask True outside the peaks
+    bkg_sel = np.full(np.shape(x), True)
+    for i,peak in enumerate(peaks):
+        width = (int(peak_FWHM[i])+1)*width_mult
+        for w in range(-width,width):
+            bkg_sel[peak+w]=False
+        w = width -1
+        while integr[peak+w] >= trend[peak+w]:
+            bkg_sel[peak+w] = False
+            w += 1
+        w = width
+        while integr[peak-w] >=trend[peak-w]:
+            bkg_sel[peak-w]=False
+            w += 1
+            
 ######################
-    #mask the source/background regions
-    sign_sel = np.zeros(np.shape(integr), dtype=bool)
-    for i in range(len(integr)):
-        for peak,width in zip(peaks,peak_FWHM):
-            if abs(i-peak) < width*width_mult:
-                sign_sel[i] = True
-    bkg_sel = ~sign_sel    
 
     #plot the luminosity profile, show source and bkg regions
     if 1 == plot_profile:
@@ -197,18 +203,17 @@ for name,file in zip(names,file_ls):
         #esimate the bkg of the filtered regions only
         bkg_est_filt = np.mean(integr[bkg_sel])
 
-        plt.axhline(y=bkg_est_filt, ls='dashed', c='grey', alpha=0.5)
-        for i in range(len(left_width)): #show all the source regions
-            plt.axvspan(left_width[i],
-                        right_width[i],
-                        alpha=0.1, color='red')
+        plt.plot(x, trend_raw+0.05*bkg_est, ls='dashed', c='grey', alpha=0.5)
+        ima = np.zeros(np.shape(bkg_sel))
+        plt.fill_between(x, ~bkg_sel*1.1*max(integr), color='red', alpha=.1)
+        
         #plt settings
         plt.ylim(min(integr)*0.9, max(integr)*1.1)
         plt.legend(['raw signal','cleaned signal',
-                    'bkg signal only', 'bkg level',
+                    'bkg signal only', 'detrended treshold',
                     f'peak regions ({width_mult}xFWHM)'])
         
-        if savefiles is True:
+        if save_plots is True:
             plt.savefig('./plots/integr/'+year+'_'+name[:-8]+'.png')
             plt.close()
         else:
@@ -219,13 +224,15 @@ for name,file in zip(names,file_ls):
     total = np.nanmean(data, axis = 0) #integration along the slit
     sky = np.nanmean(data[bkg_sel,:], axis = 0) #integration of bkg rows only
 
+    total[total == 0] = np.nan
+
     #plot the spatially integrated spectrum of the bkg
     if 1 == plot_spec:
         plt.title(year+'/'+name[:-8]+': bkg spectrum')
         plt.plot(LAMBDA, total, label='full frame', color='gray', alpha=0.3)
         plt.plot(LAMBDA, sky, label='sky only')
         plt.legend()
-        if savefiles is True:
+        if save_plots is True:
             plt.savefig('./plots/sky_spec/'+year+'_'+name[:-8]+'.png')
             plt.close()
         else:
@@ -241,14 +248,14 @@ for name,file in zip(names,file_ls):
             ma_data[i,:] = np.nan 
 
     #plot as image the bkr rows only
-    if 1 == show_ima:
+    if (1 == show_ima) and (save_plots ==0):
         plt.title('image (sky selection only)')
         plt.imshow(ma_data, extent = [LAMBDA[0], LAMBDA[-1], NAXIS2, 0])
         plt.show()
 
 ######################
     #save masked data in a new FITS file
-    if 1 == True:
+    if 1 == save_FITS:
         now = datetime.now()
         now_str = now.strftime("%Y-%m-%d %H:%M:%S")
         
@@ -262,5 +269,7 @@ for name,file in zip(names,file_ls):
         file_new = file[:-5]+'.bkg.fits'
         new_hdul.writeto(file_new, overwrite=True)
 
+f = open("bkg_extr.log", 'r')
+warnings_count = len(f.readlines())-1
 if warnings_count != 0:
     print(f'WARNING: {warnings_count} warnings occurred (see the log)')
